12f46b17c035b2073b27c9a919a08e927e929851
mmoyaerts <mathieu.moyaerts.pro@gmail.com>
2023-02-27 19:32:03 +0100

    Exercice compte bancaire

--- compte-bancaire.php
+++ compte-bancaire.php
@@ -3,6 +3,48 @@
 
 require_once('src/CompteBancaire.class.php');
 
-// TODO
+class CompteBancaire
+{
+    private $titulaire;
+    private $solde;
+    private $devise;
+
+    public function __construct($titulaire="", $solde="", $devise="EUR")
+    {
+        $this->titulaire = $titulaire;
+        $this->solde = $solde;
+        $this->devise = $devise;
+    }
+
+    public function getTitulaire()
+    {
+        return $this->titulaire;
+    }
+
+    public function getSolde()
+    {
+        return $this->solde;
+    }
+
+    public function getDevise()
+    {
+        return $this->devise;
+    }
+
+    public function afficher()
+    {
+        echo "Titulaire du compte : $this->titulaire\n";
+        echo "Vous disposez de $this->solde $this->devise\n";
+    }
+
+    public function crediter($montant)
+    {
+        $this->solde = $this->solde + $this->montant;
+    }
+    public function debiter($montant)
+    {
+        $this->solde = $this->solde - $this->montant;
+    }
+}
 
 ?>
